46cfaed8372a3c1f857e07467590c2f5904a3e4a
tzerk <christoph.burow@gmx.net>
2015-01-05 18:27:57 +0100

    improve resize js code and R implementation

--- Server.R
+++ Server.R
@@ -11,15 +11,21 @@ shinyServer(function(input, output, session) {
   val<- reactiveValues(height="100%", width="100%")
   
   observeEvent(input$dim, {
+    
     if(val$height=="100%" || val$width=="100%") {
       val$height<- val$width<- 0
     }
-    if(abs(val$height-input$dim[2]/2) > 100) {
-      val$height<- input$dim[2]/2
+    if(abs(val$height-input$dim[2]) > 100) {
+      val$height<- input$dim[2]
     }
-    if(abs(val$width-input$dim[1]/2) > 100) {
-      val$width<- input$dim[1]/2
+    if(abs(val$width-input$dim[1]) > 100) {
+      val$width<- input$dim[1]-100
     }
+  })
+  
+  output$info_timeline<- renderText({
+    paste("<center>Observation period:",stats_Luminescence$date[1],"to",stats_Luminescence$date[length(stats_Luminescence$date)],"<br>",
+          "Total downloads: 'Luminescence' =", nrow(stats_Luminescence), "| 'numOSL' =", nrow(stats_numOSL))
   })
   
   output$plot_timeline<- renderGvis({
@@ -29,19 +35,14 @@ shinyServer(function(input, output, session) {
     if(input$tl_rmean==TRUE) {
       df_Lum<- zoo(df_Lum$Freq, df_Lum$Var1)
       df_numOSL<- zoo(df_numOSL$Freq, df_numOSL$Var1)
-      
       df_Lum<- as.data.frame(rollmean(df_Lum, input$tl_rmean_val))
       df_numOSL<- as.data.frame(rollmean(df_numOSL, input$tl_rmean_val))
-      
       df_Lum<- data.frame(Date=row.names(df_Lum), Value=df_Lum[,1])
       df_numOSL<- data.frame(Date=row.names(df_numOSL), Value=df_numOSL[,1])
     }
-    
     df<- rbind(transform(df_Lum, package="Luminescence"), transform(df_numOSL, package="numOSL"))
     df<- transform(df, Title=NA, Annotation=NA)
     colnames(df)<- c("Date", "Value", "Package", "Title", "Annotation")
-    
-    
     
     gvisAnnotationChart(df, datevar="Date",
                         numvar="Value", idvar="Package",
@@ -49,7 +50,7 @@ shinyServer(function(input, output, session) {
                         options=list(displayAnnotations=TRUE,
                                      colors="['blue', 'red']",
                                      legendPosition='newRow',
-                                     width=val$width, height=val$height))
+                                     width=val$width, height=ifelse(val$width=="100%","200%",val$width/2)))
   })
   
   output$plot_map<- renderGvis({
--- www/plot_resize.js
+++ www/plot_resize.js
@@ -1,6 +1,12 @@
 window.onresize = function() {  
   dim = new Array();
+  
+  /*
   dim[0] = window.innerWidth;
   dim[1] = window.innerHeight;
+  */
+  
+  dim[0] = document.getElementById("plot_wrapper").offsetWidth;
+  dim[1] = document.getElementById("plot_wrapper").offsetHeight;
   Shiny.onInputChange('dim', dim);
-} 
+}
