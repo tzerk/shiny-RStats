704d58a29a029ff0c1a1864c1cd4e7d8c84b8cd9
tzerk <XnIASwkTCBlGgudcCsml>
2015-04-15 12:44:01 +0200

    Various minor changes & bugfixes
    * use separate select input for piechart and histogram
    * fix package download text
    * scale data of threejs globe

--- Server.R
+++ Server.R
@@ -27,36 +27,45 @@ shinyServer(function(input, output, session) {
   })
   
   output$info_timeline<- renderText({
-    paste("<center>Observation period:",stats_Luminescence$date[1],"to",stats_Luminescence$date[length(stats_Luminescence$date)],"<br>",
-          "Total downloads:",paste0("'",input$tl_package,"'")," =", nrow(get(paste0("stats_",input$tl_package))))
+    if (is.null(input$tl_package))
+      paste("<center>Please select package(s)") 
+    if (!is.null(input$tl_package)) {
+      obs <- paste("<center>Observation period:",stats_Luminescence$date[1],"to",stats_Luminescence$date[length(stats_Luminescence$date)])
+      stats <- character(length(input$tl_package))
+      for (i in 1:length(input$tl_package)) {
+        stats[i] <- paste("Total downloads:", paste0("'",input$tl_package[i],"'")," =", nrow(get(paste0("stats_", input$tl_package[i]))))
+      }
+      paste(obs, "<br>", paste(stats, collapse = "<br>"))
+    }
   })
   
   output$plot_timeline<- renderGvis({
     input$ab_plot
     isolate({
-    df<- sapply(input$tl_package, function(x){ as.data.frame(table(get(paste0("stats_",x))$date)) }, simplify = F)
-    
-    if(input$tl_rmean==TRUE) {
-      for(i in 1:length(df)) {
-        df[[i]]<- as.data.frame(rollmean(zoo(df[[i]]$Freq, df[[i]]$Var1), input$tl_rmean_val))
-        df[[i]]<- data.frame(Date=row.names(df[[i]]), Value=df[[i]][,1])
+      if (is.null(input$tl_package)) return(NULL)
+      df<- sapply(input$tl_package, function(x){ as.data.frame(table(get(paste0("stats_",x))$date)) }, simplify = F)
+      
+      if(input$tl_rmean==TRUE) {
+        for(i in 1:length(df)) {
+          df[[i]]<- as.data.frame(rollmean(zoo(df[[i]]$Freq, df[[i]]$Var1), input$tl_rmean_val))
+          df[[i]]<- data.frame(Date=row.names(df[[i]]), Value=df[[i]][,1])
+        }
       }
-    }
-    
-    df2<- data.frame()
-    for(i in 1:length(df)) {
-      df2<- rbind(df2, transform(df[[i]], package=input$tl_package[i]))
-    }
-    
-    df2<- transform(df2, Title=NA, Annotation=NA)
-    colnames(df2)<- c("Date", "Value", "Package", "Title", "Annotation")
-    
-    gvisAnnotationChart(df2, datevar="Date",
-                        numvar="Value", idvar="Package",
-                        titlevar="Title", annotationvar="Annotation",
-                        options=list(displayAnnotations=TRUE,
-                                     legendPosition='newRow',
-                                     width=val$width, height=ifelse(val$width=="100%","200%",val$width/2)))
+      
+      df2<- data.frame()
+      for(i in 1:length(df)) {
+        df2<- rbind(df2, transform(df[[i]], package=input$tl_package[i]))
+      }
+      
+      df2<- transform(df2, Title=NA, Annotation=NA)
+      colnames(df2)<- c("Date", "Value", "Package", "Title", "Annotation")
+      
+      gvisAnnotationChart(df2, datevar="Date",
+                          numvar="Value", idvar="Package",
+                          titlevar="Title", annotationvar="Annotation",
+                          options=list(displayAnnotations=TRUE,
+                                       legendPosition='newRow',
+                                       width=val$width, height=ifelse(val$width=="100%","200%",val$width/2)))
     })
   })
   
@@ -68,15 +77,14 @@ shinyServer(function(input, output, session) {
   })
   
   output$plot_pie<- renderGvis({
-    df<- arrange(as.data.frame(table(subset(get(paste0("stats_",input$geo_package)), select = input$pie_vars))), Freq, Var1)
+    df<- arrange(as.data.frame(table(subset(get(paste0("stats_",input$pie_package)), select = input$pie_vars))), Freq, Var1)
     df<- setorder(as.data.table(df), -Freq)
-    gvisPieChart(df,
-                 options=list(width=val$width, height=val$height))
+    gvisPieChart(df, options=list(width=val$width, height=val$height))
     
   })
   
   output$plot_hist<- renderGvis({
-    df<- arrange(as.data.frame(table(subset(get(paste0("stats_",input$geo_package)), select = input$hist_vars))), Var1, Freq)
+    df<- arrange(as.data.frame(table(subset(get(paste0("stats_",input$hist_package)), select = input$hist_vars))), Var1, Freq)
     df<- setorder(as.data.table(df), -Freq)
     gvisColumnChart(df, options=list(width=val$width, height=ifelse(val$width=="100%","200%",val$width/2)))
   })
@@ -87,7 +95,7 @@ shinyServer(function(input, output, session) {
     val <- arrange(as.data.frame(table(subset(get(paste0("stats_",input$globe_package)), select = "country"))), Var1, Freq)
     colnames(val) <- c("code", "Freq")
     val2 <- join(val, countryCodeCoords, by = "code")
-    args <- c(col, list(lat = val2$lat, long = val2$lon, value = val2$Freq, atmosphere = TRUE))
+    args <- c(col, list(lat = val2$lat, long = val2$lon, value = val2$Freq * input$globe_scale/100, atmosphere = TRUE))
     do.call("globejs", args = args)
   })
   
